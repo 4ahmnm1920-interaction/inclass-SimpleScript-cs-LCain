4732f4447a610e6ed2e4c42e22556ecc97a2171c
Lyra Cain <denis.cain@htl-salzburg.ac.at>
2019-12-04 11:34:48 +0100

    added Score scripts

---
+++ Assets/MyGame/Scripts/ScoreExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScore(5, "Joan Bazques");
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void SetScore(int scoreNum, string scoreName)
+	{
+		int scoreNumValue = scoreNum;
+		string scoreNameValue = scoreName;
+				
+		Debug.Log(scoreNameValue + " has scored " + scoreNumValue + " times!");
+	}
+
+
+}
